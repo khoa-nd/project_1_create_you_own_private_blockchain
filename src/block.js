24e3864413b44120a24ae62ab47ea61e485aee95
Khoa Nguyen <khoa.nguyen@pops.vn>
2021-06-14 22:14:52 +0700

    Fix issues after review

--- src/block.js
+++ src/block.js
@@ -41,7 +41,9 @@ class Block {
             // Save in auxiliary variable the current block hash
             const currentBlockHash = self.hash;
             // Recalculate the hash of the Block
+            self.hash = null;
             const calculatedBlockHash = SHA256(JSON.stringify(self)).toString();
+            self.hash = currentBlockHash;
             // Comparing if the hashes changed
             // Returning the Block is not valid
             // Returning the Block is valid
